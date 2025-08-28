2f88848e5391ec84333b2a09bca1eb3d0a287132
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-28 13:27:06 +0300

    feat: implement remove, length, clear, keys, values, and entries methods in HashMap class

--- script.js
+++ script.js
@@ -64,4 +64,55 @@ class HashMap {
     }
     return false;
   }
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        bucket.splice(i, 1);
+        this.size--;
+        this.loadFactor = this.size / this.capacity;
+        return true;
+      }
+    }
+    return false;
+  }
+  length() {
+    return this.size;
+  }
+  clear() {
+    this.capacity = 8;
+    this.size = 0;
+    this.loadFactor = 0;
+    this.buckets = Array(this.capacity)
+      .fill(null)
+      .map(() => []);
+  }
+  keys() {
+    const keysArray = [];
+    for (const bucket of this.buckets) {
+      for (const [key, value] of bucket) {
+        keysArray.push(key);
+      }
+    }
+    return keysArray;
+  }
+  values() {
+    const valuesArray = [];
+    for (const bucket of this.buckets) {
+      for (const [key, value] of bucket) {
+        valuesArray.push(value);
+      }
+    }
+    return valuesArray;
+  }
+  entries() {
+    const entriesArray = [];
+    for (const bucket of this.buckets) {
+      for (const [key, value] of bucket) {
+        entriesArray.push([key, value]);
+      }
+    }
+    return entriesArray;
+  }
 }
