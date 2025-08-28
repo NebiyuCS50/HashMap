9b720c1afce5df0a592e009598dfead4db81abdd
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-08-28 12:55:10 +0300

    feat: add hash method to HashMap class

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ class HashMap {
     this.size;
     this.buckets;
   }
+  hash(key) {
+    let hashCode = 0;
+    let prime = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (hashCode * prime + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
 }
